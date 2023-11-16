d52b8d51a5239c8c4e22c63980da087106b323ef
shaani0 <150924531+shaani0@users.noreply.github.com>
2023-11-16 08:09:35 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperaion.js
@@ -0,0 +1,19 @@
+let score = "ali"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+let isloggedIn = "hitsh"
+let booleanisloggedIn = Boolean(isloggedIn)
+//console.log(booleanisloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
