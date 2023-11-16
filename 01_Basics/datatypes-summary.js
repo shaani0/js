95d029479394816eb120f9bb8d3a5c0963e5b671
shaani0 <150924531+shaani0@users.noreply.github.com>
2023-11-17 00:20:47 +0500

    Add files via upload

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+
+// primitive datatypes(string,number,boolean,null,undefined,symbol,bigInt)
+
+const score = 100
+
+const scorevalue  = 100.3
+
+const IsloggedIn = false
+const outsideTemp = null
+
+let useremail;
+
+const id = Symbol('123');
+const anotherId = Symbol ('123');
+
+console.log(id === anotherId);
+
+const bigNumber = 6546545654n
+
+
+// Non primitive datattypes
+// Arrays , objects , functions
+
+const heros = ['superman', 'batman','Ironman'];
+let myobj = {
+    name : "shayan",
+    Age : 22,
+}
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherId );
