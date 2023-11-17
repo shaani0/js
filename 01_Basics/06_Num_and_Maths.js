ed96924e76a4f61bd4035035c9de67d292ee4416
shaani0 <150924531+shaani0@users.noreply.github.com>
2023-11-17 14:03:08 +0000

    Num and strings

---
+++ 01_Basics/06_Num_and_Maths.js
@@ -0,0 +1,30 @@
+const score = 100
+
+//console.log(score);
+
+const balance = new Number(200)
+
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const othernumber = 127.765
+
+//console.log(othernumber.toPrecision(4));
+
+const hundreds = 1000000
+
+//console.log(hundreds.toLocaleString());
+
+
+//++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
