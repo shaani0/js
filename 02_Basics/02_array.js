947962b4866bd9789929e018eb27a6b8a26b82b2
shaani0 <150924531+shaani0@users.noreply.github.com>
2023-11-18 08:00:36 +0000

    array2

---
+++ 02_Basics/02_array.js
@@ -0,0 +1,34 @@
+const marvel_heros = ["thor","flash","ironman"]
+const dc_heros = ["spiderman","superman","Batman"]
+
+//  marvel_heros.push(dc_heros)
+
+//  console.log(marvel_heros);
+//  console.log(marvel_heros[3][2]);
+
+//  const allheros = marvel_heros.concat(dc_heros)
+
+//  console.log(allheros);
+
+//  const all_new_heros = [...marvel_heros, ...dc_heros]
+
+//  console.log(all_new_heros);
+
+// const another_arr = [1,2,3, [4,5],6,7, [5,8,[9,8]],9,4]
+
+// console.log(another_arr.flat(Infinity));
+
+console.log(Array.isArray("hitesh"));
+console.log(Array.from("hitesh"));
+console.log(Array.from({name: "hitesh"}));
+
+
+let score1 = 100
+let score2 = 200
+let score3= 300
+
+console.log(Array.of(score1,score2,score3));
+
+const name = ['s','h','a','y','a','n']
+
+console.log(name[3]);
