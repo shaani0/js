ef1db43f03210b0d81b07956e27fa11c1b497e0d
shaani0 <150924531+shaani0@users.noreply.github.com>
2023-11-18 07:29:24 +0000

    array 1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,41 @@
+const myarr = [0,1,2,3,4,5]
+
+// console.log(myarr);
+// console.log(myarr[3])
+
+// myarr.push(6)
+// console.log(myarr);
+
+// myarr.pop()
+// console.log(myarr);
+
+// myarr.unshift(9)
+
+// console.log(myarr);
+
+// myarr.shift()
+
+// console.log(myarr)
+
+// console.log(myarr.includes(4));
+
+// console.log(myarr.indexOf(8));
+
+
+// const newarr = myarr.join()
+
+// console.log(myarr);
+// console.log(newarr);
+// console.log(typeof newarr);
+
+
+console.log("A", myarr);
+
+const myn1 = myarr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myarr);
+
+const myn2 = myarr.splice(1,3)
+console.log("C",myarr);
+console.log(myn2);
