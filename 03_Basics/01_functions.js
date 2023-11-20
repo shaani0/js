95a8b02647a14b271dfcb465a3fa4807696d01d4
shaani0 <150924531+shaani0@users.noreply.github.com>
2023-11-20 19:21:51 +0000

    functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -41,10 +41,40 @@ function myname(){
 function addnumbers(num1 , num2){
 
     let rs = num1 + num2
-    console.log(num1 + num2);
+    // console.log(num1 + num2);
     return rs
     // return num1 + num2
 }
 const rs = addnumbers(3,7)
-console.log("result is :", rs );
-console.log(rs);
+// console.log("result is :", rs );
+// console.log(rs);
+
+
+function calculatecartprice(val1, ...numbr1){
+
+return numbr1
+}
+console.log(calculatecartprice(200, 700, 800));
+
+const user = {
+    username: "hitesh",
+    prices : 299
+}
+function handleAnyobject(anyobject){
+    console.log(`username is ${anyobject.username}, and the price is ${anyobject.price}`);
+}
+
+// handleAnyobject(user)
+handleAnyobject({
+    username : "khan",
+    price : "399"
+})
+
+const newArray = [200, 400, 500]
+
+function returnSecvalue(getArray){
+
+return getArray[2]
+}
+// console.log(returnSecvalue(newArray));
+console.log(returnSecvalue([200, 400, 500,1000]));
