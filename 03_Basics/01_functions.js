36e7c08cf2aa1ba3e3809715d5665068dc9d9eea
shaani0 <150924531+shaani0@users.noreply.github.com>
2023-11-20 14:06:14 +0000

    functions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,50 @@
+
+
+function myname(){
+    console.log("H");
+    console.log("i");
+    console.log("t");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+}
+
+// myname()   To execute function
+
+// function addtwonumbers(number1 , number2){
+
+// console.log(number1 + number2);
+// // }
+// function addtwonumbers(number1 , number2){
+
+//     // let result = number1 + number2
+//     // return result
+// return  number1 + number2
+//     }
+// const result = addtwonumbers(3,5)
+
+// console.log("result :", result );
+
+
+// function UserloginMessage(username = "sam"){
+//     if(!username){
+//         console.log("please enter a username");
+//         return
+
+//     }
+//     return `${username} just logged in`
+// }
+
+// console.log(UserloginMessage("khan"));
+
+
+function addnumbers(num1 , num2){
+
+    let rs = num1 + num2
+    console.log(num1 + num2);
+    return rs
+    // return num1 + num2
+}
+const rs = addnumbers(3,7)
+console.log("result is :", rs );
+console.log(rs);
