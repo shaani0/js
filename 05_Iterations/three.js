e16f3c67c2e533acb0621d1c9ae64dfd9d92b6aa
shaani0 <150924531+shaani0@users.noreply.github.com>
2023-11-22 20:06:55 +0000

    For of loop

---
+++ 05_Iterations/three.js
@@ -0,0 +1,36 @@
+// FOR OF LOOP
+
+// [" "," "," "] 
+// [{},{},{}]
+
+// const array = [1,2,3,4,5]
+
+// for (const Num of array) {
+//     // console.log(Num);
+// }
+
+// const greeting = "Hello world!"
+
+// for (const greet of greeting) {
+//     console.log(`every single word ${greet}`);
+// }
+
+// MAPS
+
+const map = new Map()
+
+map.set('IN', "india")
+map.set('US', "United states of America")
+map.set('Uk', "London")
+// console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key ,':-', value);
+}
+
+const myobject = {
+
+    game1 = 'NFS',
+    game = 'IGI'
+
+}
