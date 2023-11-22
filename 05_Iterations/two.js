f9b0ceb2950ff5561995b18870ec8354fec347f4
shaani0 <150924531+shaani0@users.noreply.github.com>
2023-11-22 19:27:02 +0000

    while_Do-while

---
+++ 05_Iterations/two.js
@@ -0,0 +1,26 @@
+// WHILE LOOP 
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is : ${index}`);
+//     index = index +2
+
+// }
+
+// let myarray = ["flash","Batman","Superman"]
+
+// let arr = 0
+
+// while (arr < myarray.length) {
+//     console.log(`value is ${myarray[arr]}`);
+//     arr = arr + 1
+// }
+
+// DO-WHILE LOOP
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
