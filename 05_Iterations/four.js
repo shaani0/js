1d0d8af9c8418072b4c6406753b8bde9e34c727d
shaani0 <150924531+shaani0@users.noreply.github.com>
2023-11-22 20:12:57 +0000

    For in loop

---
+++ 05_Iterations/four.js
@@ -0,0 +1,11 @@
+const myobject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myobject) {
+   console.log(`${key} shortcut is ${myobject[key]}`);
+        
+    }
